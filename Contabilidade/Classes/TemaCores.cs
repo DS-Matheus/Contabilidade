37c6748a2b43267703e4609a2e9ff9bd5f71282b
Matheus Dutra da Silva <ds-matheus@outlook.com>
2025-01-20 07:42:04 -0300

    Alteração nas cores do tema do painel de lançamentos para uma melhor visibilidade

--- Contabilidade/Classes/TemaCores.cs
+++ Contabilidade/Classes/TemaCores.cs
@@ -18,9 +18,10 @@ namespace Contabilidade.Models
         // Dicionário associando índices a listas de strings (agora com cores em hexadecimal)
         private static Dictionary<string, List<string>> dicionarioStrings = new Dictionary<string, List<string>>
         {
+            // Barra de títulos, Botões menus, Botões sub-menus, Fundo painel lateral
             { "padrão", new List<string> { "#777787", "#33334C", "#3D3D5B", "#27263A" } },
             { "cadastros", new List<string> { "#794ee6", "#362367", "#4d3291", "#20153c" } },
-            { "lançamentos", new List<string> { "#188066", "#105544", "#20aa87", "#082a22" } },
+            { "lançamentos", new List<string> { "#20AA87", "#105544", "#188066", "#082a22" } },
             { "relatórios", new List<string> { "#d4176a", "#3b2144", "#6e1e51", "#082438" } },
             { "logoff", new List<string> { "#cc4040", "#003f5c", "#2c4875", "#00202e" } },
         };
